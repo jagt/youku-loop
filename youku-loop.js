23429c0b28e851a8c1b200d489980f0325d01c1f
jagt <jagt@live.com>
2013-03-26 23:36:23 +0800

    use youku provided api for loop; fix icon display

--- youku-loop.js
+++ youku-loop.js
@@ -1,28 +1,18 @@
 javascript: void(
 (function() {
 /* TODO add adjustment widgets maybe */
-/* TODO movie player still accept nsseek when paused,
- *      so maybe it doesn't need to loop so frequently */
 /* IE6 has a limit of 508 chars of bookmarklet, so better
  * keep compressed length under 500
  * use prototype.js for better size */
-var embed = $('movie_player');
-if (!embed.getNsData || $('_ykl_oop_')) return; 
-var _loophandler = function() {
-    var data = embed.getNsData();
-    if (parseFloat(data.alltime) - parseFloat(data.time) < 1.1) {
-        embed.nsseek(0);
-        embed.PlayerResume();
-        /* although we can resume player, youku plays an ad at
-         * the end of video. so basically we can't let the
-         * video finish, we have to test for ending earlier */
+
+if (!setPlayerLoop || $('_ykl_oop_')) return;
+/* Youku v5 player just support loop and continious play */
+var set_loop = function(loop) {
+    setPlayerLoop(loop);
+    if (loop) {
+        PlayerContinuous(false);
     }
 };
-var interval_id = setInterval(_loophandler, 500);
-
-/* disable continouse play, so it won't jump to another
- * page before _loophandler is called */
-if ( typeof setPf === 'function' ) setPf({'checked':false});
 
 /* icon display and events */
 var loop_icon = new Element('span',
@@ -33,10 +23,11 @@ var loop_icon = new Element('span',
 loop_icon.update('&#8734; '); /*nbsp show as a ? in IE6*/
 
 /* insert in this way is more generic and should fit all types of pages*/
-$$('h1 span')[0].insert({'after' : loop_icon});
+$$('h1').first().insert(loop_icon);
+set_loop(true);
 
 loop_icon.observe("click", function(event) {
-    clearInterval(interval_id);
+    set_loop(false);
     loop_icon.remove();
 });
 })()
